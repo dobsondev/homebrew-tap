8045461a17cd3ce44b80b5f870348ab39e34b593
DobsonDev <alex@dobsondev.com>
2026-06-23 22:00:00 -0600

    bugfix: bad sha

--- Formula/desktop-file-helper.rb
+++ Formula/desktop-file-helper.rb
@@ -3,7 +3,7 @@ class DesktopFileHelper < Formula
   desc "A helper for working with desktop files on Bazzite"
   homepage "https://github.com/dobsondev/homebrew-tap"
   url "https://github.com/dobsondev/homebrew-tap/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "4796d6e36a9f426432580ac3f750bade054f345cbae7fffe2f3fb5f50bba02b0"
 
   def install
     bin.install "scripts/desktop-file-helper.sh" => "desktop-file-helper"
